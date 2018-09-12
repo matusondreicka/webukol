aff0149dd298a566065f787366a3576895531fa7
mondrei <matus.ondreicka@gmail.com>
2018-09-12 22:29:06 +0200

    get greeting endpoint and angular + bootstrap

---
+++ src/main/resources/static/controller.js
@@ -0,0 +1,37 @@
+var appMain = angular.module('app', ['ui.bootstrap']);
+
+appMain.controller("MainController", function($scope, $q, $http, $timeout){
+
+    var main = this;
+
+    main.data = ["eeeeeee"];
+
+    main.load2 = function () {
+        main.data = ["eeeeeee", main.data];
+    };
+
+    main.load = function () {
+
+// https://webukol.herokuapp.com/greeting
+// '/greeting'
+        $http.get('/greeting').
+        success(function(data, status, headers, config) {
+
+            console.log(" /greeting #### : "+data);
+
+            main.data = data;
+
+        }).
+        error(function(data, status, headers, config) {
+
+            console.log("/info ####  error= "+error);
+
+        });
+
+
+    };
+
+
+
+});
+
